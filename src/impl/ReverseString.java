b38b70642a117843fa0de91278627ebf16a4b3a4
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2016-10-25 09:52:27 +0200

    Reverse String Problem

--- src/impl/ReverseString.java
+++ src/impl/ReverseString.java
@@ -1,9 +1,13 @@
 package impl;
 
 public class ReverseString {
-	
+	/**
+	 * Use StringBuilder instead of String. It is significantly faster than the normal string operations. 
+	 * @param s
+	 * @return
+	 */
 	public static String reverseString(String s) {
-        String reverse = "";
+//      String reverse = "";
         StringBuilder sb = new StringBuilder();
         for (int i = s.length()-1; i >= 0; i--) {
 //			reverse += s.charAt(i);
