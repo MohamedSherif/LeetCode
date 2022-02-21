2694eeb0e08df980688c1cbf017f07c384462d34
Mohamed Sherif <m.sherif@scopegroup.com>
2022-02-21 01:26:29 +0100

    Data Structure Implementations
    - Min Heap Array Imp.
    - LinkedList

---
+++ src/microsoft/LinkedList.java
@@ -0,0 +1,57 @@
+package microsoft;
+
+public class LinkedList {
+	Node head;
+
+	public void append(int data) {
+		if(head == null) {
+			head = new Node(data);
+			return;
+		}
+		
+		Node current = head;
+
+		while (current.next != null) {
+			current = current.next;
+		}
+
+		current.next = new Node(data);
+	}
+
+	public void prepend(int data) {
+		Node newHead = new Node(data);
+		
+		newHead.next = head;
+		head = newHead;
+	}
+
+	public void deleteWithValue(int data) {
+		if(head == null) return;
+		
+		if(head.data == data) {
+			head = head.next;
+			return;
+		}
+		
+		Node current = head;
+		
+		while(current.next != null) {
+			if(current.next.data == data) {
+				current.next = current.next.next;
+				return;
+			}
+			current = current.next;
+		}
+	}
+	
+	public static class Node {
+
+		Node next;
+		int data;
+
+		public Node(int data) {
+			// TODO Auto-generated constructor stub
+			this.data = data;
+		}
+	}
+}
